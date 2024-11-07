acd8e868f031966f860a41e0fc0cc53fe834ade8
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-11-07 09:12:10 +0100

    imported font files and image file.

--- FruitDemo/MauiProgram.cs
+++ FruitDemo/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace FruitDemo
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Bold.ttf", "Bold");
+                    fonts.AddFont("Roboto-Regular.ttf", "Regular");
                 });
 
 #if DEBUG
